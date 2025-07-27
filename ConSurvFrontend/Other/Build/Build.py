9475a48110e38f85cbe26b75b86be6c65549c6d4
Marius Göcke <marius.goecke@gmail.com>
2025-07-27 21:37:45 +0200

    extracted messages

--- ConSurvFrontend/Other/Build/Build.py
+++ ConSurvFrontend/Other/Build/Build.py
@@ -1,10 +1,18 @@
-import sys
 from pathlib import Path
+from ScriptCollection.GeneralUtilities import GeneralUtilities
 from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
 
 
+def standardized_tasks_build_for_angular_extract_i18n(t: TasksForCommonProjectStructure, scriptfile: str):
+    codeunit_folder: str = GeneralUtilities.resolve_relative_path("../../..", scriptfile)
+    t.run_with_epew("ng", "extract-i18n --format=xlf2", codeunit_folder)
+
+
 def build():
-    TasksForCommonProjectStructure().standardized_tasks_build_for_angular_codeunit(str(Path(__file__).absolute()), "QualityCheck",  1, sys.argv)
+    t = TasksForCommonProjectStructure()
+    file = str(Path(__file__).absolute())
+    # t.standardized_tasks_build_for_angular_codeunit(file, "QualityCheck",  1, sys.argv)
+    standardized_tasks_build_for_angular_extract_i18n(t, file)
 
 
 if __name__ == "__main__":
