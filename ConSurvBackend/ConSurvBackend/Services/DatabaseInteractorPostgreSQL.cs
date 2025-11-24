31e379e784e8dd89b6e3082e6bba1014a7b9e586
Marius Göcke <marius.goecke@gmail.com>
2025-11-24 22:50:06 +0100

    removed log from db interactor

--- ConSurvBackend/ConSurvBackend/Services/DatabaseInteractorPostgreSQL.cs
+++ ConSurvBackend/ConSurvBackend/Services/DatabaseInteractorPostgreSQL.cs
@@ -8,13 +8,6 @@ namespace ConSurvBackend.Core.Services
 {
     public class DatabaseInteractorPostgreSQL : IConSurvDatabaseInteractor
     {
-        public IGRYLog Log
-        {
-            get
-            {
-                return this._DatabaseInteractor.Log;
-            }
-        }
         private readonly PostgreSQLDatabaseInteractor _DatabaseInteractor;
         private readonly IList<MigrationInstance> _Migrations = GRYMigrator.LoadMigrationsFromResources(Assembly.GetExecutingAssembly(), "ConSurvBackend.Core.Resources.Database.PostgreSQL.Migrations.");
         public DatabaseInteractorPostgreSQL(IGenericDatabaseInteractor interactor)
