c0df1270c764f4cad8d4a46550aba3d9733abb8f
Marius Göcke <marius.goecke@gmail.com>
2026-02-08 21:51:03 +0100

    updates for image-loading

--- ConSurv/Other/UpdateDependencies.py
+++ ConSurv/Other/UpdateDependencies.py
@@ -2,10 +2,9 @@ from ScriptCollection.TFCPS.Docker.TFCPS_CodeUnitSpecific_Docker import TFCPS_Co
 
 
 def update_dependencies():
-    tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)    
+    return#TODO
+    tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
     tf.tfcps_Tools_General.update_images_in_example(tf.get_codeunit_folder(),["consurv"])
-    #TODO update image in example
-    
+
 if __name__ == "__main__":
     update_dependencies()
- 
