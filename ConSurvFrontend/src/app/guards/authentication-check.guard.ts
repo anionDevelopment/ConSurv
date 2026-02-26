99e69d7a7ace7248716527bd5e364f9e3b5ed1ff
Marius Göcke <marius.goecke@gmail.com>
2026-02-26 22:58:49 +0100

    update for nginx config

--- ConSurvFrontend/src/app/guards/authentication-check.guard.ts
+++ ConSurvFrontend/src/app/guards/authentication-check.guard.ts
@@ -8,7 +8,7 @@ export const authenticationCheckGuard: CanActivateFn = (route, state) => {
   const router: Router = inject(Router);
   return userDataService.userIsLoggedIn().pipe(tap((userIsLoggedIn) => {
     if (!userIsLoggedIn) {
-      router.navigate(['']);
+      router.navigate(['/']);
     }
     return userIsLoggedIn;
   }));
