f03ada4025cf5d01eead5bb52740f546442395b6
Marius Göcke <marius.goecke@gmail.com>
2026-03-28 11:58:24 +0100

    update

--- ConSurv/Other/UpdateDependencies.py
+++ ConSurv/Other/UpdateDependencies.py
@@ -3,7 +3,7 @@ from ScriptCollection.TFCPS.Docker.TFCPS_CodeUnitSpecific_Docker import TFCPS_Co
 
 def update_dependencies():
     tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
-    tf.tfcps_Tools_General.update_images_in_example(tf.get_codeunit_folder(),["consurv"])
+    tf.tfcps_Tools_General.update_images_in_example(tf.get_codeunit_folder(),["consurv"],None)
 
 if __name__ == "__main__":
     update_dependencies()
